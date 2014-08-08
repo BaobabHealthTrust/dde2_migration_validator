265ecf3785cef8143b9ff8590af2f86d7e5088dc
Blessings <blessings.mhango@gmail.com>
2014-08-09 00:58:00 +0200

    removed session

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,19 +1,19 @@
 class PeopleController < ApplicationController
   def index
     @people_count = Person.count 
-   
+    
     national_id = params[:npid]
     unless national_id.blank?
     	dde2_person = Person.find(national_id) rescue nil
       dde_person = DdeNationalPatientIdentifier.find_by_value(national_id).person rescue nil
       if !dde2_person.blank? && !dde_person.blank?
-       session[:dde2_person] = nil
-       session[:dde_person] = nil
        @dde2_person = dde2_person
        @dde_person = dde_person  
       elsif dde2_person.blank?
+        @dde_person = dde_person
         @error = "DDE2 does not have a person with a national id of : #{national_id}"
-      elsif dde_person.blank?  
+      elsif dde_person.blank?
+        @dde2_person = dde2_person
         @error = "DDE1 does not have a person with a national id of : #{national_id}" 
       end 
     else

2910029742a2c6c9e3dc2e6eb3853c8dd0919792
Blessings <blessings.mhango@gmail.com>
2014-08-11 19:59:34 +0200

    working on migration report

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'people/index'
+  get 'people/premigration'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

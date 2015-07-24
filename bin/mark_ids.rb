d2972478c4889894af5bc25b4f04b54c341621da
Blessings <blessings.mhango@gmail.com>
2015-07-24 11:51:12 +0200

    marking ids

--- bin/mark_ids.rb
+++ bin/mark_ids.rb
@@ -6,16 +6,18 @@ dde_ids.each do |identifier|
 
 		found_id = Npid.by_national_id.keys([identifier.value])
 
-		if found_id.present?
-			if found_id.assigned == false
+		if found_id.present? && found_id.assigned == false
+			
 			  site_code = DdeSite.find(identifier.assigner_site_id).code
 			  region = Site.find(site_code).region
+			  
 				found_id.update_attributes(:assigned => true, 
 																	 :site_code => site_code, 
 																	 :region => region)
 				logger.info found_id.national_id.to_s
+				
 				puts "Updated " + found_id.national_id.to_s
-			end
+			
 		end
 	
 end

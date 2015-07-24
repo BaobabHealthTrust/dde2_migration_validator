4bfcd5c75691d246f39cd27256b3f7b3db798dba
Blessings <blessings.mhango@gmail.com>
2015-07-24 11:49:41 +0200

    added new script for marking ids

---
+++ bin/mark_ids.rb
@@ -0,0 +1,21 @@
+logger = Logger.new(Rails.root.join("log","marked_ids.log"))
+
+dde_ids = DdeNationalPatientIdentifier.all
+
+dde_ids.each do |identifier|
+
+		found_id = Npid.by_national_id.keys([identifier.value])
+
+		if found_id.present?
+			if found_id.assigned == false
+			  site_code = DdeSite.find(identifier.assigner_site_id).code
+			  region = Site.find(site_code).region
+				found_id.update_attributes(:assigned => true, 
+																	 :site_code => site_code, 
+																	 :region => region)
+				logger.info found_id.national_id.to_s
+				puts "Updated " + found_id.national_id.to_s
+			end
+		end
+	
+end
